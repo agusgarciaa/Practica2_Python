a1e5d89b3ff0c2cbfea05d76f8235b7d5279a6f1
Agustin Garcia <agusgarciaa23@gmail.com>
2026-04-02 00:59:27 -0300

    Agregadas las funciones del ejercicio 1.

--- src/funciones.py
+++ src/funciones.py
@@ -1,3 +1,32 @@
-def contar_lineas(texto): 
-    lineas = texto.split("\n")
-    return len(lineas)
+def count_lines(text): 
+    lines = text.split("\n")
+    return len(lines)
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def average_words(text):
+    total = 0
+    lines = text.split("\n")
+    
+    for line in lines:
+        total += count_words(line)
+        
+    div = count_lines(text)
+    average = total/div
+    return average
+
+
+def above_average(text):
+    filtered_lines = []
+    average = average_words(text)
+    lines = text.split("\n")
+    
+    for line in lines:
+        if count_words(line) > average:
+            filtered_lines.append(line)
+            
+    return filtered_lines
