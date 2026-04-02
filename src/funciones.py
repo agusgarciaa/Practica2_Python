0513a56c7d37ff784ed4e798fbef044cdf6e60c0
Agustin Garcia <agusgarciaa23@gmail.com>
2026-04-02 01:56:55 -0300

    Agregadas las funciones del ejercicio 2.

--- src/funciones.py
+++ src/funciones.py
@@ -30,3 +30,47 @@ def above_average(text):
             filtered_lines.append(line)
             
     return filtered_lines
+
+
+def to_seconds(duration):
+    mins, secs = duration.split(":")
+    mins = int(mins)
+    secs = int(secs)
+    total_seconds = mins * 60 + secs
+    return total_seconds
+
+
+def song_playtime(song):
+    time = song["duration"]
+    total_seconds = to_seconds(time)
+    return total_seconds
+
+
+def total_playtime(playlist):
+    total = 0
+    for song in playlist:
+        total += song_playtime(song)
+    return total
+        
+
+def max_playtime(playlist):
+    max_playtime = 0
+    for song in playlist:
+        duration = song_playtime(song)
+        
+        if duration > max_playtime:
+            max_playtime = duration
+            max_song = song
+    return max_song
+        
+
+def min_playtime(playlist):
+    min_playtime = 999999
+    for song in playlist:
+        duration = song_playtime(song)
+        
+        if duration < min_playtime:
+            min_playtime = duration
+            min_song = song
+    return min_song
+        
